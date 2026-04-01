faaff9a0cee87e3e167f171f06d30299bce2a71c
Renzo Damian <renzo.damian.go@gmail.com>
2026-04-01 21:09:50 +0000

    Fixed base footprint link origin

---
+++ my_rb1_description/launch/display.launch.py
@@ -0,0 +1,36 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.substitutions import Command
+
+def generate_launch_description():
+
+    urdf_file = os.path.join(
+        get_package_share_directory('my_rb1_description'),
+        'urdf',
+        'my_rb1_robot.urdf'
+    )
+
+    with open(urdf_file, 'r') as f:
+        robot_description = f.read()
+
+    return LaunchDescription([
+
+        Node(
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui',
+        ),
+
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            parameters=[{'robot_description': robot_description}],
+        ),
+
+        Node(
+            package='rviz2',
+            executable='rviz2',
+        ),
+
+    ])
